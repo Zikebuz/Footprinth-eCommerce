3567e2931aa2da4f17fd111fab3e08200736c269
Zikebuz <71839001+Zikebuz@users.noreply.github.com>
2022-11-20 03:05:10 -0800

    Updates to styles and the header component

--- component/Header.js
+++ component/Header.js
@@ -4,7 +4,7 @@ import { FaShoppingCart } from 'react-icons/fa';
 
 function Header() {
 return <div className="layout-header">
-    <h2 className="brand">Footprint</h2>
+    <h2 className="brand">Footprinth</h2>
     <p>
       <a className="snipcart-checkout snipcart-summary cart-link" href="cart">
         <FaShoppingCart /> <span className="snipcart-total-price custom-cart">NGN 0.00</span>
